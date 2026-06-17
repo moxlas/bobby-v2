3e420f4ec2981807c7e84261cde6a8a8c64bbe32
moxlas <moxlas420@gmail.com>
2026-06-18 00:32:04 +0100

    Update main.tsx
    adding themes

--- main.tsx
+++ main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./globals.css";
+import { loadSettings } from "./utils/settings";
+
+const saved = loadSettings();
+if (saved?.options.theme && saved.options.theme !== 'summer') {
+  document.documentElement.dataset.theme = saved.options.theme;
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
